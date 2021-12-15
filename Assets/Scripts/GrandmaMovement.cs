e2a899641c10e0122d5b984d6aa9f7920eda4b27
Jake Tsuchiyama <jtsuchiyama@scu.edu>
2021-12-14 14:29:19 -1000

    Properly coded the movement system to execute movements one at a time

--- Assets/Scripts/GrandmaMovement.cs
+++ Assets/Scripts/GrandmaMovement.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,84 +8,114 @@ public class GrandmaMovement : MonoBehaviour
     public Text displayText;
 
     private Rigidbody2D rb;
+
+    // String property that holds the commands that need to be executed
     private string _commandString = "";
 
-    // Editable fields for the speed in the Unity editor
+    // Boolean flag that indicates if movement is occuring
+    private bool _move = false;
+
+    // Editable fields for the speed in the Unity editor.
     [SerializeField] private float horiSpeed;
     [SerializeField] private float vertSpeed;
 
-    // Initializes variables before application starts
+    // Initializes variables before application starts.
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Runs every frame
+    // Runs every frame.
     private void Update()
     {
         InputHandler();
     }
 
-    // Receives and executes the input
+    // Runs in time with the physics system.
+    private void FixedUpdate()
+    {
+        MoveHandler();
+    }
+
+    // Receives the input and adds it to the _commandString
     private void InputHandler()
     {
-        // Checks if there was input
+        // If a move is being executed, then input should not be accepted.
+        if (_move == true)
+            return;
+
+        // Checks if there was input.
         string input = Input.inputString;
         if (input == "")
             return;
 
-        // Checks if the "run command" key was inputted
+        // Checks if the "run command" key was inputted.
         char charInput = input[0];
         if (charInput == '\n' || charInput == '\r')
         {
-            ExecuteCommands();
+            _move = true;
             return;
         }
 
-        // Checks if a directional key was inputted
+        // Checks if a directional key was inputted.
         if (!(charInput == 'q' || charInput == 'w' || charInput == 'e' || charInput == 'a' || charInput == 'd'))
             return;
 
-        // Ensures no more than 5 commands are inputted
+        // Ensures no more than 5 commands are inputted.
         if (_commandString.Length < 5)
             _commandString += charInput;
         displayText.text = _commandString;
     }
 
-
-    // Runs the commands that have been inputted
-    private void ExecuteCommands()
+    // Detects when movement should occur and executes the movements
+    private void MoveHandler()
     {
-        // Iterates through each character in the command string to run each command
-        foreach (char c in _commandString)
+        // If no moves need to be executed, then end the call.
+        if (_move == false)
+            return;
+
+        // If there are no moves to be made, then end the movement and allow inputs
+        if (_commandString.Length == 0)
         {
-            if (c == 'q')
-                Move(-horiSpeed, vertSpeed);
-
-            else if (c == 'w')
-                Move(rb.velocity.x, vertSpeed);
-
-            else if (c == 'e')
-                Move(horiSpeed, vertSpeed);
-
-            else if (c == 'a')
-                Move(-horiSpeed, rb.velocity.y);
-
-            else if (c == 'd')
-                Move(horiSpeed, rb.velocity.y);
+            _move = false;
+            return;
         }
 
-        // Resets the command string
-        _commandString = "";
+        // Moves should be executed once the character comes to a complete stop
+        if (rb.velocity.x == 0 && rb.velocity.y == 0)
+        {
+            MoveLogic();
+        }
+    }
+
+    // Processes the command to movement logic
+    private void MoveLogic()
+    {
+        char c = _commandString[0];
+        if (c == 'q')
+            Move(-horiSpeed, vertSpeed);
+
+        else if (c == 'w')
+            Move(rb.velocity.x, vertSpeed);
+
+        else if (c == 'e')
+            Move(horiSpeed, vertSpeed);
+
+        else if (c == 'a')
+            Move(-horiSpeed, rb.velocity.y);
+
+        else if (c == 'd')
+            Move(horiSpeed, rb.velocity.y);
+
+        // Moves to the next command
+        _commandString = _commandString.Substring(1);
         displayText.text = _commandString;
     }
 
-    // Moves the character depending on horizontal and vertical speed
+    // Moves the character depending on horizontal and vertical speed.
     private void Move(float horizontalSpeed, float verticalSpeed)
     {
         rb.velocity = new Vector2(horizontalSpeed, verticalSpeed);
-        //Vector2 force = new Vector2(horizontalSpeed*Time.deltaTime, verticalSpeed*Time.deltaTime);
-        //rb.AddForce(force);
     }
 }
 
