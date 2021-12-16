5e88a52af2772a5cbf7a102c77e1d2bf3408606f
Jake Tsuchiyama <jtsuchiyama@scu.edu>
2021-12-15 14:22:06 -1000

    Implemented the dialogue system

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    // Variables for targeting the text objects.
+    public Text nameText;
+    public Text dialogueText;
+
+    private Queue<string> sentences;    
+
+    // Start is called before the first frame update.
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    // Loads the dialogue into the dialogue manager and displays the first sentence.
+    public void StartDialogue(Dialogue dialogue)
+    {
+        nameText.text = dialogue.name;
+        
+        sentences.Clear();
+
+        // Loads all sentences.
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+
+    }
+
+    // Displays the next sentence.
+    public void DisplayNextSentence()
+    {
+        Debug.Log(sentences.Count);
+
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    // Animates the typing of the sentence.
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+
+        // Iterates through each character in the sentence, updating the dialogue and pausing per each iteration.
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    // Indicates that the dialogue has ended.
+    private void EndDialogue()
+    {
+        return;
+    }
+}
--- Assets/Scripts/GrandmaMovement.cs
+++ Assets/Scripts/GrandmaMovement.cs
@@ -9,13 +9,13 @@ public class GrandmaMovement : MonoBehaviour
 
     private Rigidbody2D rb;
 
-    // String property that holds the commands that need to be executed
+    // String property that holds the commands that need to be executed.
     private string _commandString = "";
 
-    // Boolean flag that indicates if movement is occuring
+    // Boolean flag that indicates if movement is occuring.
     private bool _move = false;
 
-    // Editable fields for the speed in the Unity editor.
+    // Editable private fields for the speed in the Unity editor.
     [SerializeField] private float horiSpeed;
     [SerializeField] private float vertSpeed;
 
@@ -37,7 +37,7 @@ public class GrandmaMovement : MonoBehaviour
         MoveHandler();
     }
 
-    // Receives the input and adds it to the _commandString
+    // Receives the input and adds it to the _commandString.
     private void InputHandler()
     {
         // If a move is being executed, then input should not be accepted.
@@ -67,28 +67,28 @@ public class GrandmaMovement : MonoBehaviour
         displayText.text = _commandString;
     }
 
-    // Detects when movement should occur and executes the movements
+    // Detects when movement should occur and executes the movements.
     private void MoveHandler()
     {
         // If no moves need to be executed, then end the call.
         if (_move == false)
             return;
 
-        // If there are no moves to be made, then end the movement and allow inputs
+        // If there are no moves to be made, then end the movement and allow inputs.
         if (_commandString.Length == 0)
         {
             _move = false;
             return;
         }
 
-        // Moves should be executed once the character comes to a complete stop
+        // Moves should be executed once the character comes to a complete stop.
         if (rb.velocity.x == 0 && rb.velocity.y == 0)
         {
             MoveLogic();
         }
     }
 
-    // Processes the command to movement logic
+    // Processes the command to movement logic.
     private void MoveLogic()
     {
         char c = _commandString[0];
@@ -107,7 +107,7 @@ public class GrandmaMovement : MonoBehaviour
         else if (c == 'd')
             Move(horiSpeed, rb.velocity.y);
 
-        // Moves to the next command
+        // Moves to the next command.
         _commandString = _commandString.Substring(1);
         displayText.text = _commandString;
     }
